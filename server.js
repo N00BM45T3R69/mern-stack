becedbf9e303ac862e7034bd62c27e6fa1fa19ea
abh1r4m <2010030523@klh.edu.in>
2023-02-25 21:04:04 +0530

    yes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5010;
 
 app.get("/",function(req,res){
-    res.send("Hello!")
+    // res.send("Hello!")
     res.sendFile(__dirname+"/index.html")
 });
 
